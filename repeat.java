fbdcb46889a256bca4324b4d379a55932004fd99
deva07 <41105274+deva07@users.noreply.github.com>
2018-07-22 11:07:45 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,17 @@
+package Deva;
+
+import java.util.Scanner;
+
+public class Repeat {
+	public static void main(String[] args)
+	{
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+
+}
